68157e1e82ac6264242db6bfe48d356e9f376021
iwat <iwatth@gmail.com>
2017-06-25 20:55:44 +0700

    rearrangestr to use shared Heap class

--- src/main/java/misc/rearrangestr/Solution.java
+++ src/main/java/misc/rearrangestr/Solution.java
@@ -2,6 +2,8 @@ package misc.rearrangestr;
 
 import java.util.*;
 
+import shared.*;
+
 public class Solution {
     public String rearrange(String input) {
         Map<Character, Integer> histogram = new HashMap();
@@ -55,94 +57,4 @@ public class Solution {
             return freq - another.freq;
         }
     }
-
-
-    static class Heap<T extends Comparable<T>> {
-        int multiplier;
-        List<T> data = new ArrayList();
-
-        public Heap(int multiplier) {
-            this.multiplier = multiplier;
-        }
-
-        void add(T value) {
-            data.add(value);
-            heapifyUp();
-        }
-
-        boolean isEmpty() {
-            return data.size() == 0;
-        }
-
-        T peek() {
-            if (data.isEmpty()) {
-                return null;
-            }
-            return data.get(0);
-        }
-
-        T poll() {
-            T head = data.get(0);
-            data.set(0, data.get(data.size() - 1));
-            data.remove(data.size() - 1);
-            heapifyDown();
-            return head;
-        }
-
-        int size() {
-            return data.size();
-        }
-
-        void heapifyUp() {
-            int index = data.size() - 1;
-            int parent = (index - 1) / 2;
-
-            while (index > 0) {
-                if (multiplier * (data.get(parent).compareTo(data.get(index))) <= 0) {
-                    break;
-                }
-
-                T tmp = data.get(parent);
-                data.set(parent, data.get(index));
-                data.set(index, tmp);
-
-                index = parent;
-                parent = (index - 1) / 2;
-            }
-        }
-
-        void heapifyDown() {
-            int index = 0;
-            int left = index*2 + 1;
-            int right = index*2 + 2;
-
-            while (left < data.size()) {
-                int diffLeft = multiplier * (data.get(left).compareTo(data.get(index)));
-                int diffRight = 0;
-
-                if (right < data.size()) {
-                    diffRight = multiplier * (data.get(right).compareTo(data.get(index)));
-                }
-
-                int next = -1;
-
-                if (diffLeft < 0 && diffLeft <= diffRight) {
-                    next = left;
-                } else if (diffRight < 0 && diffRight <= diffLeft) {
-                    next = right;
-                } else {
-                    break;
-                }
-
-                T tmp = data.get(next);
-                data.set(next, data.get(index));
-                data.set(index, tmp);
-
-                index = next;
-
-                left = index*2 + 1;
-                right = index*2 + 2;
-            }
-        }
-	}
 }
